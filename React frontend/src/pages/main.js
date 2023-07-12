2ac065dca12d235496580c4a67125753a2047dde
SehgalMonika <monika.-@stud.fra-uas.de>
2023-07-12 19:42:31 +0200

    Main page added

--- React frontend/src/pages/main.js
+++ React frontend/src/pages/main.js
@@ -8,7 +8,17 @@ const Main = () => {
   return (
     <div className="page-container mainpage">
         
+    <div class="box">
+            <h1>Pet Detection</h1>
 
+<p>This is our project representation on Clod Computing SoSe 2023.</p>
+            <h2>Main components on our poject will be displayed in the following sections:</h2>
+            <ul>
+            <li> <a href="/initial">Organisation </a></li>
+            <li> <a href="/train"> Train Images</a> </li>
+            </ul>
+            <p>Team Members: Binit Bambhroliya, Klea Maloku, Krishna Borisagar, Monika, Binyam Tekeste, Alberto Diez</p>
+            </div>
 
             
 
